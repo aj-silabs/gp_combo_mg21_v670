761df1e0ac64f586132d62312a71932d77f3105e
aj-silabs <allen.ji@silabs.com>
2020-01-28 18:03:33 +0800

    Green Power Combo - Vim

---
+++ GNU ARM v7.2.1 - Default/sources.mk
@@ -0,0 +1,78 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+S79_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S79_UPPER_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+adc/EFR32 \
+aes-cmac \
+antenna-stub \
+basic \
+. \
+ccm-encryption \
+coexistence-stub/EFR32 \
+counters \
+debug-basic-library/EFR32 \
+debug-jtag/EFR32 \
+eeprom \
+efr32 \
+ember-minimal-printf \
+emdrv \
+emlib \
+external-generated-files \
+find-and-bind-initiator \
+find-and-bind-target \
+green-power-client \
+green-power-common \
+green-power-server \
+groups-server \
+hal-library/EFR32 \
+heartbeat \
+identify \
+interpan \
+level-control \
+mac-address-filtering \
+mbedtls/EFR32 \
+network-creator \
+network-creator-security \
+network-steering \
+nvm3/EFR32 \
+nvm3 \
+on-off \
+ota-bootload \
+ota-client \
+ota-client-policy \
+ota-common \
+ota-storage-common \
+ota-storage-simple \
+ota-storage-simple-eeprom \
+packet-handoff \
+reporting \
+scan-dispatch \
+scenes \
+serial/EFR32 \
+serial \
+simple-main \
+slot-manager \
+strong-random \
+update-tc-link-key \
+zcl-framework-core \
+zll-commissioning-common \
+zll-commissioning-server \
+zll-identify-server \
+zll-on-off-server \
+zll-scenes-server \
+zll-utility-server \
+
